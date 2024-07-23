36b672b2adbd0ae67327f04246cfc0ac128bf600
Yihang Wang <wyhliverpoolfc@gmail.com>
2024-07-23 16:39:23 +1000

    feat: complete the PerfectPay project in Dot NET MAUI Course

--- Dot-NET-MAUI-Course/PerfectPay/PerfectPay/MainPage.xaml.cs
+++ Dot-NET-MAUI-Course/PerfectPay/PerfectPay/MainPage.xaml.cs
@@ -2,23 +2,71 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        decimal bill;
+        int tip;
+        int noPersons = 1;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void txtBill_Completed(object sender, EventArgs e)
         {
-            count++;
+            bill = decimal.Parse(txtBill.Text);
+            CalculateTotal();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            tip = (int)sldTip.Value;
+            lblTip.Text = $"Tip: {tip}%";
+            CalculateTotal();
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button) 
+            {
+                var btn = (Button)sender;
+                var percentage = int.Parse(btn.Text.Replace("%", "")); 
+                sldTip.Value = percentage;
+            }
+        }
+
+        private void btnMinus_Clicked(object sender, EventArgs e)
+        {
+            if (noPersons > 1) 
+            {
+                noPersons--;
+            }
+            lblNoPersons.Text = noPersons.ToString();
+            CalculateTotal();
+        }
+
+        private void btnPlus_Clicked(object sender, EventArgs e)
+        {
+            noPersons++;
+            lblNoPersons.Text = noPersons.ToString();
+            CalculateTotal();
+        }
+
+        private void CalculateTotal()
+        {
+            // Total tip
+            var  totalTip = (bill * tip) / 100;
+
+            // Tip by pserson
+            var tipByPerson = totalTip / noPersons;
+            lblTipByPerson.Text = $"{tipByPerson:C}";
+
+            // Subtotal
+            var subtotal = bill / noPersons;
+            lblSubtotal.Text = $"{subtotal:C}";
+
+            // Total
+            var totalByPerson = (totalTip + bill) / noPersons;
+            lblTotal.Text = $"{totalByPerson:C}";
         }
     }
 
